b12ebe6ea4e8e908f8ad0f756df711bccc826c84
caioflores <caioflores123@gmail.com>
2014-11-08 16:12:23 -0200

    back + template

---
+++ client/helpers/progress.js
@@ -0,0 +1,29 @@
+Template.progress.helpers({
+	progressNow: function() {
+
+		var transactions = Transactions.find().fetch();
+
+		var userGoal = Meteor.user().profile.goal;
+
+		var total = 0;
+
+		for (var i=0; i < transactions.length; i++)
+			total += transactions[i].save;
+
+		var teste = (total*100)/userGoal;
+
+		return teste;
+	},
+
+	totalSaved: function() {
+		var total = 0;
+
+		var transactions = Transactions.find().fetch();
+
+		for (var i=0; i < transactions.length; i++)
+			total += transactions[i].save; 
+		
+		return total;
+	},
+
+});
---
+++ server/server.js
@@ -0,0 +1,33 @@
+Meteor.startup(function () {
+
+	if (Meteor.users.find().count() === 0) {
+		Accounts.createUser({username: "Judit", password: "judit123", profile: {type: "precavido", deadline: "31/12/2014", goal: 10000}});
+	} 
+
+	if (Transactions.find().count() === 0) {
+		var names = ["Submarino",
+		             "MCDonalds",
+		             "Wallmart",
+		             "Cinema",
+		             "Cantina",
+		             "Nike"];
+		var dates = ["11/11/11",
+		             "12/12/12",
+		             "01/01/01",
+		             "02/02/02",
+		             "03/03/03",
+		             "04/04/04"];
+		var values = [100,
+		        200,
+		        300,
+		        4000,
+		        500,
+		        7000];
+
+		//User "conservador"
+		var userTypeFactor = 10;
+
+		for (var i = 0; i < names.length; i++)
+		 	Transactions.insert({name: names[i], date: dates[i], value: values[i], save: (values[i]*userTypeFactor)/100});
+	}
+});
