8172d337f0b317b35d84d4ae55358cf357694a55
caioflores <caioflores123@gmail.com>
2014-11-08 14:22:57 -0200

    transactions and user preferences

---
+++ client/client.js
@@ -0,0 +1,36 @@
+Meteor.startup(function () {
+	if (Meteor.users.find().count() === 0) {
+		Meteor.users.insert({name: "Judit", password: "", profile: {type: "precavido", deadline: "31/12/2014", goal: "Carro"}});
+		Meteor.loginWithPassword("Judit", "");
+	} 
+  
+	if (Transactions.find().count() === 0) {
+		var names = ["Submarino",
+		             "MCDonalds",
+		             "Wallmart",
+		             "Cinema",
+		             "Cantina",
+		             "Nike"];
+		var dates = ["11/11/11",
+		             "12/12/12",
+		             "01/01/01",
+		             "02/02/02",
+		             "03/03/03",
+		             "04/04/04"];
+		var values = [100,
+		        200,
+		        300,
+		        4000,
+		        500,
+		        7000];
+
+		//User "conservador"
+		var userTypeFactor = 10;
+
+		//User Goal
+		var userGoal = 1000;
+
+		for (var i = 0; i < names.length; i++)
+		 	Transactions.insert({name: names[i], date: dates[i], value: values[i], save: (values[i]*userTypeFactor)/100});
+	}
+});
