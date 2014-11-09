020c805d000088f6900d2c5b5f16867ba7714298
augustolazaro <aemlazaro@gmail.com>
2014-11-09 01:38:37 -0200

    sweet alert

--- client/helpers/progress.js
+++ client/helpers/progress.js
@@ -23,6 +23,26 @@ Template.progress.helpers({
 		
 		return total;
 	},
+	progressComplete: function() {
+		var transactions = Transactions.find().fetch();
+		var userGoal = Meteor.user().profile.goal.price;
+		var total = 0;
+
+		for (var i=0; i < transactions.length; i++)
+			total += transactions[i].save;
+
+		var teste = (total*100)/userGoal;
+
+		if (teste >= 100) {
+			swal({
+				title: "Parabéns!", 
+				text: "Sua meta foi alcançada!", 
+				type: "success",
+				timer: 3000,
+				confirmButtonColor: "#0F9D58"
+			})
+		};
+	}
 
 
 
