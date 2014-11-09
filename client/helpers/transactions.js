4c9c958317bdd62ad4813e7bc3c6d782507c6bfa
Bruno Lemos <brunohplemos@gmail.com>
2014-11-09 14:43:31 -0200

    Presentation Version

--- client/helpers/transactions.js
+++ client/helpers/transactions.js
@@ -1,3 +1,7 @@
+Template.transactions.rendered = function() {
+	this.$('#transactions .item').addClass('no-effect');
+}
+
 Template.transactions.helpers({
 	transactions: function () {
 	  return Transactions.find({}, {sort: {date: -1}, limit: 4});
